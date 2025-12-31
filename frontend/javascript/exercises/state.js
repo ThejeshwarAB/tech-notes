0fa6c7ec6a1f290728ee16598e1f6c07e393dd8d
Thejeshwar AB <abibastheja@gmail.com>
2025-12-31 05:48:19 +0530

    changes in state js

--- frontend/javascript/exercises/state.js
+++ frontend/javascript/exercises/state.js
@@ -24,12 +24,14 @@ var obj2 = obj1;
 console.log("Object 1 Key: " + obj1.key);
 console.log("Object 2 Key: " + obj2.key);
 
+// modification
 console.log("Modifying obj1.key to 'value2'");
 obj1.key = "value2";
 
 console.log("Object 1 Key: " + obj1.key);
 console.log("Object 2 Key: " + obj2.key);
 
+// reassignment
 console.log("Reassigning obj2 to a new object");
 obj2 = { key: "value3" };
 
