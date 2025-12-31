530b5135388ad897e68ed377002e3e9e800dac0d
Thejeshwar AB <abibastheja@gmail.com>
2025-12-31 05:36:19 +0530

    add state js

---
+++ frontend/javascript/exercises/state.js
@@ -0,0 +1,37 @@
+var a = 5;
+console.log(a);
+
+a = 10;
+console.log(a);
+
+//object
+var person = {
+  name: "John",
+};
+console.log(person.name);
+
+person.name = "Doe";
+console.log(person.name);
+
+person= {
+  name: "Alice",
+};
+console.log(person.name);
+
+var obj1 = { key: "value1" };
+var obj2 = obj1;
+
+console.log("Object 1 Key: " + obj1.key);
+console.log("Object 2 Key: " + obj2.key);
+
+console.log("Modifying obj1.key to 'value2'");
+obj1.key = "value2";
+
+console.log("Object 1 Key: " + obj1.key);
+console.log("Object 2 Key: " + obj2.key);
+
+console.log("Reassigning obj2 to a new object");
+obj2 = { key: "value3" };
+
+console.log("Object 1 Key: " + obj1.key);
+console.log("Object 2 Key: " + obj2.key);
